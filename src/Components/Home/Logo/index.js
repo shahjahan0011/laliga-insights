3a06fe41db4b1fd34919cd0613ea38b144460c7c
Jahan Shah <85148797+shahjahan0011@users.noreply.github.com>
2025-08-05 18:17:49 -0700

    optimizing for mobile screen

--- src/Components/Home/Logo/index.js
+++ src/Components/Home/Logo/index.js
@@ -1,9 +1,33 @@
 import './index.scss';
+import { useEffect, useState } from 'react';
 import laliga from '../../../assets/laliga.png';
 
 const Logo = () => {
+  const [showMobileWarning, setShowMobileWarning] = useState(false)
+
+  useEffect(() => {
+    // Check if the screen width is less than 768px (typical mobile width)
+    if (window.innerWidth <= 768) {
+      setShowMobileWarning(true)
+    }
+  }, [])
+
+  const handleClosePopup = () => {
+    setShowMobileWarning(false)
+  }
 
   return (
+    <>
+    {showMobileWarning && (
+        <div className="mobile-warning-overlay">
+          <div className="mobile-warning-popup">
+            <p>The website is not optimized for mobile devices. </p> 
+            <p>Please view it on a larger screen.</p>
+            <button onClick={handleClosePopup}>OK</button>
+          </div>
+        </div>
+      )}
+
     <div className="logo-container">
       <img
         className="solid-logo"
@@ -11,6 +35,7 @@ const Logo = () => {
         alt="Logo J"
       />
     </div>
+    </>
   )
 }
 
