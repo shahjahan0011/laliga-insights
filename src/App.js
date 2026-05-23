49ba5df14524d4129313db5acbf3c0077ff00bbb
Jahan Shah <85148797+shahjahan0011@users.noreply.github.com>
2026-05-22 17:27:29 -0700

    Revert to original App.js - restore data fetching functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout';
 import Home from './Components/Home';
 import Teams from './Components/Teams';
 import Positions from './Components/Positions';
 import Search from './Components/Search';
 import Country from './Components/Country';
+import Api from './Components/Api';
 import { Analytics } from "@vercel/analytics/react"
 
 function App() {
@@ -15,11 +16,10 @@ function App() {
     <Route path="/" element={<Layout />} > 
     <Route index element={<Home />}/>
     <Route path="/teams" element={<Teams />}/>
+    <Route path='/data' element={<Api />}/>
     <Route path="/country" element={<Country />}/>
     <Route path="/position" element={<Positions />}/>
     <Route path="/search" element={<Search />}/>
-    {/* Catch-all route - redirects undefined paths to home */}
-    <Route path="*" element={<Navigate to="/" replace />} />
     </Route>
     </Routes>
     <Analytics />
